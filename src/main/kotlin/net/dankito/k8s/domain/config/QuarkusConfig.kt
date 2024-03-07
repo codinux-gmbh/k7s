76b070f5f6f0c1ba83041500b406f24c7c0ee95f
dankito <git@dankito.net>
2024-03-07 22:17:49 +0100

    Added remaining classes required for Quarkus Native

--- src/main/kotlin/net/dankito/k8s/domain/config/QuarkusConfig.kt
+++ src/main/kotlin/net/dankito/k8s/domain/config/QuarkusConfig.kt
@@ -3,15 +3,13 @@ package net.dankito.k8s.domain.config
 import io.quarkus.runtime.annotations.RegisterForReflection
 import net.dankito.k8s.api.dto.HomePageData
 import net.dankito.k8s.api.dto.ResourceItemsViewData
-import net.dankito.k8s.domain.model.KubernetesResource
-import net.dankito.k8s.domain.model.ResourceItem
-import net.dankito.k8s.domain.model.ResourceItems
+import net.dankito.k8s.domain.model.*
 
 @RegisterForReflection(
     targets = [
         // Quarkus won't find them in native mode otherwise
         HomePageData::class, ResourceItemsViewData::class,
-        KubernetesResource::class, ResourceItem::class, ResourceItems::class
+        KubernetesResource::class, ResourceItems::class, ResourceItem::class, PodResourceItem::class, ContainerStatus::class
     ]
 )
 class QuarkusConfig
