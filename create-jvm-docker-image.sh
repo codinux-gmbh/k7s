32d377817dddb82d30fdb829852c20226e13e0d1
dankito <git@dankito.net>
2024-03-17 21:29:35 +0100

    Added kubectl to Docker images

--- create-jvm-docker-image.sh
+++ create-jvm-docker-image.sh
@@ -1,6 +1,13 @@
 
 ./gradlew build -x test &&
 
+if [ ! -f build/apps/kubectl ]; then
+  echo "Downloading kubectl to build/apps/kubectl ..."
+  mkdir -p "build/apps"
+  # wget is installed on more systems than curl
+  wget "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -O build/apps/kubectl
+fi
+
 docker build -f src/main/docker/Dockerfile.jvm -t docker.dankito.net/dankito/k7s-jvm . &&
 
 docker push docker.dankito.net/dankito/k7s-jvm
--- create-native-docker-image.sh
+++ create-native-docker-image.sh
@@ -1,6 +1,13 @@
 
 ./gradlew build -Dquarkus.package.type=native -x test &&
 
+if [ ! -f build/apps/kubectl ]; then
+  mkdir -p "build/apps"
+  echo "Downloading kubectl to build/apps/kubectl ..."
+  # wget is installed on more systems than curl
+  wget "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -O build/apps/kubectl
+fi
+
 docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/k7s . &&
 
 docker push docker.dankito.net/dankito/k7s
