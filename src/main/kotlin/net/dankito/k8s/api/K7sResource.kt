1a97e801d65bdc5c57140b3f64f859ad7e5dbf9f
dankito <git@dankito.net>
2025-09-17 03:59:36 +0200

    Consistently named all resource paths 'resources'

--- src/main/kotlin/net/dankito/k8s/api/K7sResource.kt
+++ src/main/kotlin/net/dankito/k8s/api/K7sResource.kt
@@ -17,20 +17,20 @@ class K7sResource(
     fun getAllAvailableResourceTypes(@RestQuery("context") context: String? = null) =
         service.getAllAvailableResourceTypes(context)
 
-    @Path("resource/{resourceName}/{namespace}/{itemName}/yaml")
+    @Path("resources/{resourceName}/{namespace}/{itemName}/yaml")
     @GET
     @Produces("application/yaml")
     fun getResourceItemYaml(@RestPath("resourceName") resourceName: String, @RestPath("namespace") namespace: String?, @RestPath("itemName") itemName: String, @RestQuery("context") context: String? = null): String? {
         return service.getResourceItemYaml(resourceName, namespace?.takeUnless { it.isBlank() || it == "null" }, itemName, context)
     }
 
-    @Path("resource/{resourceName}/{namespace}/{itemName}") // TODO: why doesn't "resources/{namespace}/{itemName}" work?
+    @Path("resources/{resourceName}/{namespace}/{itemName}") // TODO: why doesn't "resources/{namespace}/{itemName}" work?
     @DELETE
     fun deleteResourceItem(@RestPath("resourceName") resourceName: String, @RestPath("namespace") namespace: String?, @RestPath("itemName") itemName: String, @RestQuery("context") context: String? = null) {
         service.deleteResourceItem(resourceName, namespace?.takeUnless { it.isBlank() || it == "null" }, itemName, context)
     }
 
-    @Path("resource/{resourceName}/{namespace}/{itemName}")
+    @Path("resources/{resourceName}/{namespace}/{itemName}")
     @PATCH
     fun deleteResourceItem(@RestPath("resourceName") resourceName: String, @RestPath("namespace") namespace: String?, @RestPath("itemName") itemName: String, @RestQuery("context") context: String? = null, @RestQuery("scaleTo") scaleTo: Int? = null) {
         service.patchResourceItem(resourceName, namespace?.takeUnless { it.isBlank() || it == "null" }, itemName, context, scaleTo)
