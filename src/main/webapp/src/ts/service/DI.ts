9054f6bdf57db9233b97e204434b93dab048ac17
dankito <git@dankito.net>
2025-09-17 03:26:28 +0200

    Configured WebClient for development and production

--- src/main/webapp/src/ts/service/DI.ts
+++ src/main/webapp/src/ts/service/DI.ts
@@ -1,7 +1,36 @@
+import type {WebClient} from "../clients/web/WebClient"
+import {FetchWebClient} from "../clients/web/FetchWebClient"
 import {LogService} from "./LogService"
 
 export class DI {
 
   static readonly log: LogService = new LogService()
+
+  static readonly webClient: WebClient = new FetchWebClient(DI.getBaseUrl(), DI.log, `Access Log Visualizer client; ${navigator.userAgent}`)
+
+
+  private static getBaseUrl(): string {
+    let baseUrl = DI.determineBackendHost()
+    const port = import.meta.env.VITE_BACKEND_PORT
+
+    if (port) {
+      baseUrl += ":" + port
+    }
+
+    baseUrl += "/access-log/api/v1"
+
+    return baseUrl
+  }
+
+  private static determineBackendHost(): string {
+    const configuredHost = import.meta.env.VITE_BACKEND_HOST
+    if (configuredHost) {
+      return configuredHost
+    }
+
+    const fileUrl = new URL(import.meta.url)
+
+    return `${fileUrl.protocol}//${fileUrl.hostname}`
+  }
 
 }
