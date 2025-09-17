734a2973e227e5c3847898eac42e1b8d12f2e509
dankito <git@dankito.net>
2025-09-17 08:31:17 +0200

    Moved /resources to base path

--- src/main/kotlin/net/dankito/k8s/api/K7sResource.kt
+++ src/main/kotlin/net/dankito/k8s/api/K7sResource.kt
@@ -6,32 +6,31 @@ import net.dankito.k8s.domain.service.KubernetesService
 import org.jboss.resteasy.reactive.RestPath
 import org.jboss.resteasy.reactive.RestQuery
 
-@Path("/api/v1")
+@Path("/api/v1/resources")
 @Produces(MediaType.APPLICATION_JSON)
 class K7sResource(
     private val service: KubernetesService
 ) {
 
     @GET
-    @Path("resources")
     fun getAllAvailableResourceTypes(@RestQuery("context") context: String? = null) =
         service.getAllAvailableResourceTypes(context)
 
     @GET
-    @Path("resources/{resourceName}/{namespace}/{itemName}/yaml")
+    @Path("/{resourceName}/{namespace}/{itemName}/yaml")
     @Produces("application/yaml")
     fun getResourceItemYaml(@RestPath("resourceName") resourceName: String, @RestPath("namespace") namespace: String?, @RestPath("itemName") itemName: String, @RestQuery("context") context: String? = null): String? {
         return service.getResourceItemYaml(resourceName, namespace?.takeUnless { it.isBlank() || it == "null" }, itemName, context)
     }
 
     @DELETE
-    @Path("resources/{resourceName}/{namespace}/{itemName}") // TODO: why doesn't "resources/{namespace}/{itemName}" work?
+    @Path("/{resourceName}/{namespace}/{itemName}") // TODO: why doesn't "resources/{namespace}/{itemName}" work?
     fun deleteResourceItem(@RestPath("resourceName") resourceName: String, @RestPath("namespace") namespace: String?, @RestPath("itemName") itemName: String, @RestQuery("context") context: String? = null) {
         service.deleteResourceItem(resourceName, namespace?.takeUnless { it.isBlank() || it == "null" }, itemName, context)
     }
 
     @PATCH
-    @Path("resources/{resourceName}/{namespace}/{itemName}")
+    @Path("/{resourceName}/{namespace}/{itemName}")
     fun scaleResourceItem(@RestPath("resourceName") resourceName: String, @RestPath("namespace") namespace: String?, @RestPath("itemName") itemName: String, @RestQuery("context") context: String? = null, @RestQuery("scaleTo") scaleTo: Int? = null) {
         service.scaleResourceItem(resourceName, namespace?.takeUnless { it.isBlank() || it == "null" }, itemName, context, scaleTo)
     }
