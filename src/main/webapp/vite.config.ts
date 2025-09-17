a2596f2e1c9d2b62cdd49588443d23c221d6cc25
dankito <git@dankito.net>
2025-09-17 03:27:48 +0200

    Enabled hot module reload

--- src/main/webapp/vite.config.ts
+++ src/main/webapp/vite.config.ts
@@ -5,7 +5,11 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    svelte(),
+    svelte({
+      compilerOptions: {
+        hmr: true,
+      },
+    }),
     tailwindcss(),
   ],
   server: {
