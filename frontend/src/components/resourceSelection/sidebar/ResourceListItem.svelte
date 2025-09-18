3f1ce2ab5331fbe697a2e7aebd309188d103cce5
dankito <git@dankito.net>
2025-09-18 04:17:25 +0200

    Fixed that button didn't use full width

--- frontend/src/components/resourceSelection/sidebar/ResourceListItem.svelte
+++ frontend/src/components/resourceSelection/sidebar/ResourceListItem.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <li class="min-h-8 p-1 pl-6 pr-0 leading-6 cursor-pointer lg:hover:bg-zinc-200">
-  <button class="text-start" onclick={() => service.selectedResourceChanged(resource)}>
+  <button class="w-full text-start" onclick={() => service.selectedResourceChanged(resource)}>
     { title }
   </button>
 </li>
