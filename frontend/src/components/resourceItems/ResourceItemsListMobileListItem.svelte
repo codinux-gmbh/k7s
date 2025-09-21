4ac417757e3935e552515d489d2784a7d90213f9
dankito <git@dankito.net>
2025-09-21 11:48:30 +0200

    Extracted ResourceItemsListMobileListItem

---
+++ frontend/src/components/resourceItems/ResourceItemsListMobileListItem.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import ResourceItemsListContextMenu from "./ResourceItemsListContextMenu.svelte"
+  import {ResourceItem} from "../../ts/model/ResourceItem"
+  import {DI} from "../../ts/service/DI"
+  import {KubernetesResource} from "../../ts/model/KubernetesResource"
+
+  let { item, resource, showNamespace }: { item: ResourceItem, resource: KubernetesResource, showNamespace: boolean } = $props()
+
+  const itemsFormatter = DI.itemsFormatter
+
+
+  function getItemStyle(item: ResourceItem): string {
+    return itemsFormatter.getItemStyle(item) ?? ""
+  }
+</script>
+
+
+<div class={[ "w-full flex items-stretch min-h-[3.25rem] box-border border-b first:border-t border-zinc-200 even:bg-zinc-100/50 hover:bg-zinc-200/50",
+               getItemStyle(item) ]}>
+  <div class="grow p-2 pr-0 flex flex-col justify-center overflow-hidden">
+    <div class="flex items-center">
+      {#if showNamespace}<div class="flex-none max-w-[5.5rem] md:max-w-[12rem] mr-1 font-medium truncate">{item.namespace}</div>{/if}
+      <div class="flex-1 truncate">{item.name}</div>
+
+      {#each item.highlightedItemSpecificValues as value}
+        {#if value.showOnMobile}
+          <div class="flex-none max-w-[8rem] ml-1 truncate">{#if value.mobileValue}{value.mobileValue}{:else}{value.name}: {value.value}{/if}</div>
+        {/if}
+      {/each}
+    </div>
+
+    {#if item.secondaryItemSpecificValues}
+      <div class="w-full flex items-center mt-2 text-xs box-border">
+        {#each item.secondaryItemSpecificValues as value}
+          {#if value.showOnMobile}
+            <div class={[ value.useRemainingSpace ? "flex-1" : "flex-none max-w-[8rem]", "mr-1 truncate" ]}>
+              {#if value.mobileValue}{value.mobileValue}
+              {:else}<span class="font-medium">{value.name}</span>: {value.value}{/if}
+            </div>
+          {/if}
+        {/each}
+      </div>
+    {/if}
+  </div>
+
+  <div class="shrink-0 w-9 ml-1 p-0 flex items-center">
+    <ResourceItemsListContextMenu {item} {resource} />
+  </div>
+</div>
