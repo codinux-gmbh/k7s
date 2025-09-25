7516eed57c8fb8a2a7731c4bc02bcf64b053f171
dankito <git@dankito.net>
2025-09-25 13:52:24 +0200

    Also running now bun/npm run buildQuarkusDist with Exec task, could therefore remove node plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.gradle.node.npm.task.NpxTask
 import org.gradle.kotlin.dsl.support.serviceOf
 import java.io.ByteArrayOutputStream
 
@@ -7,8 +6,6 @@ plugins {
     kotlin("plugin.allopen")
 
     id("io.quarkus")
-
-    id("com.github.node-gradle.node") version "7.1.0"
 }
 
 
@@ -78,26 +75,27 @@ tasks.withType<Test> {
 
 
 val isBunInstalled = isProgramInstalled("bun")
+val bunOrNpm = if (isBunInstalled) "bun" else "npm"
+val frontendFolder = file("$projectDir/frontend")
 
-// com.github.node-gradle.node plugin's NpmInstallTask did not work as it only supports package.json files in root source directory (honestly?)
+// com.github.node-gradle.node plugin's NpmInstallTask did not work as it only supports package.json files in root source directory (honestly node plugin?)
 val npmInstallFrontend = tasks.register<Exec>("npmInstallFrontend") {
     group = "frontend"
     description = "Run bun/npm install to install frontend dependencies if required. Ensures e.g. that vite is available for buildFrontend task"
 
-    workingDir = File("$projectDir/frontend")
+    workingDir = frontendFolder
 
-    commandLine(if (isBunInstalled) "bun" else "npm", "install")
+    commandLine(bunOrNpm, "install")
 }
 
-val buildFrontendTask = tasks.register<NpxTask>("buildFrontend") {
+val buildFrontendTask = tasks.register<Exec>("buildFrontend") {
     dependsOn(npmInstallFrontend)
     group = "frontend"
     description = "Transpiles Svelte components to standard JavaScript and CSS files and copies them to src/resources/META-INF/resources"
 
-    workingDir.set(File("$projectDir/frontend"))
+    workingDir = frontendFolder
 
-    command.set(if (isBunInstalled) "bun" else "npm")
-    args.addAll("run", "buildQuarkusDist")
+    commandLine(bunOrNpm, "run", "buildQuarkusDist")
 }
 
 tasks.named("processResources") {
